de7cbbdae6d0331beae8b345c11ee0ee76b4a295
02-ahmed <ahmedboabae2@gmail.com>
2024-02-24 11:54:37 +0000

    renamed the folder for lesson 2

---
+++ part1/lesson 2 exercises - numbers and math/index.js
@@ -0,0 +1,22 @@
+//2a. At a restaurant, you order 1 soup for $10, 3 burgers for $8 each, and 1 ice cream for $5. Use JavaScript to calculate the cost of the order.
+let soup = 10;
+let burger = (8) * 3;
+let iceCream = 5;
+
+total = soup + burger + iceCream;
+console.log(`total is $${total}`);
+//====================================================================================================================
+
+//2b. You're at a restaurant with 2 friends (3 people in total) and make the same order as 2a. Calculate how much each person pays.
+let people = 3;
+let eachPerson = total / people;
+console.log(`each person pays $${eachPerson}`)
+
+//====================================================================================================================
+//2c. Calculate the total cost of a toaster ($18.50) and 2 shirts ($7.50 each).
+
+
+//2d. Calculate a 10% tax for the total in exercise 2c.
+
+
+//2e. Calculate a 20% tax for the total in 2c (remember that 1% = 1/100,so 20% = 20 / 100 = 0.2).
