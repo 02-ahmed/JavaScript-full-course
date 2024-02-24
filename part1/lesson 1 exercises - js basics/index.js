59af4f3a16aa300290badd604661855add356abe
02-ahmed <ahmedboabae2@gmail.com>
2024-02-24 10:55:28 +0000

    completed exercise 1

---
+++ part1/lesson 1 exercises - js basics/index.js
@@ -0,0 +1,40 @@
+//use alert to display 'good morning' in a popup
+alert('Good morning!');
+
+//display your name in a popup
+alert('Ahmed');
+
+//using math, calculate 10 + 5 in the console
+console.log(10 + 5);
+
+//calculate 20-5 in the console
+console.log(20 - 5);
+
+//calculate 2+2-5 in the console
+console.log(2+2-5);
+
+//use document.body.innerHTML to display 'good morning' on the web page
+document.body.innerHTML = "Good morning";
+
+
+//----------------------challenge exercises------------------
+// you order a t shirt for $10, socks for $8 and plates for $20. Use js to calculate the total cost
+let shirt = 10;
+let socks = 8;
+let plates = 20;
+let total = shirt + socks + plates;
+console.log(`Total is $${total}`);
+//------------------------------------------------
+
+
+//Your bank account has $100, you spend $20 on lunch, $50 on dinner, and earn $200 from your job. how much money do you have
+let account = 100;
+let lunch = 20;
+let dinner = 50;
+let income = 200;
+let balance = (account - lunch - dinner) + income;
+console.log(`You have $${balance}`);
+
+
+//Use document.body.innerHTML to make the webpage blank
+//document.body.innerHTML = "";
