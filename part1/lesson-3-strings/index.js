06482e5f20e7e207e37554dab93906d2e0870e06
02-ahmed <ahmedboabae2@gmail.com>
2024-02-24 14:29:53 +0000

    added and completed lesson 3 challenge exercises

--- part1/lesson-3-strings/index.js
+++ part1/lesson-3-strings/index.js
@@ -22,3 +22,33 @@ console.log(tempString);
 
 //3f. Display the text from 3e in a popup using alert()
 alert(tempString);
+console.log(`
+.
+.
+challenge exercises`)
+//=============challenge exercises=============
+/*Setup: in the Amazon project
+update the cart to 2 basketballs ($20.95 each) with $4.99 shipping,
+and 2 t-shirts($7.99 each) with $4.99 shipping.*/
+
+//3k. Using interpolation, create the first line of text (use math to calculate the numbers 4 and 57.88).
+let items = 4;
+let price = ((2*2095) + (2*799))/100;
+let shipping = (499 * 2)/100;
+let orderTotal = (price + shipping);
+let tax = (10/100 * orderTotal).toFixed(2);
+let msg = `Items(${items}): $${price}`
+console.log(msg)
+
+
+//3l. Create second line of text: 'Shipping & handling: $9.98' (use math).
+let msg2 = `Shipping & handling: $${shipping}`;
+console.log(msg2);
+
+//3m. Create third line: 'Total before tax: $67.86' (use math).
+let msg3 = `Total before tax: $${orderTotal}`;
+console.log(msg3);
+
+//3n. Create fourth line of text: 'Estimated tax (10%): $6.79' (use math and Math.round...); to calculate the exact number).
+let msg4 = `Estimated tax (10%): $${tax}`;
+console.log(msg4);
