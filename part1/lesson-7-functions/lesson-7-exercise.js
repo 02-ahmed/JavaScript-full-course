e2a6259c8b5a1c0f6f327dde077ed2df3063594f
02-ahmed <ahmedboabae2@gmail.com>
2024-02-26 20:02:21 +0000

    completed lesson 7 exercise 7d

--- part1/lesson-7-functions/lesson-7-exercise.js
+++ part1/lesson-7-functions/lesson-7-exercise.js
@@ -3,3 +3,12 @@ function greet(name) {
 }
 
 greet();
+
+//======================================================================
+ function convertToFahrenheit(celsius) {
+  let fahrenheit = (celsius * 9 / 5) + 32;
+  return `${fahrenheit}F`;
+ }
+
+let conversion1 = convertToFahrenheit(25);
+console.log(conversion1)
