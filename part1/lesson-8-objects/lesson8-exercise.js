ec7ad19d868fa8bcf89771030dc2d0258c8dcddb
02-ahmed <ahmedboabae2@gmail.com>
2024-02-28 19:32:34 +0000

    completed exercise 8d

--- part1/lesson-8-objects/lesson8-exercise.js
+++ part1/lesson-8-objects/lesson8-exercise.js
@@ -1,14 +1,37 @@
 let amount = 2095;
 const product = {
   name : 'basketball' ,
-  price : `${amount} cents` ,
+  price : 2095 ,
+  
 }
 console.log(product);
 
 //=============================
-product.price = `${amount + 500} cents`;
+product.price += 500;
 console.log(product);
 
 //================================
 product['delivery-time'] = '3 days';
 console.log(product);
+
+//================================
+
+const product1 = {
+  name: 'noodles' ,
+  price: 10 ,
+}
+const product2 = {
+  name: 'spaghetti' ,
+  price: 5 ,
+}
+
+console.log(comparePrice(product1, product2));
+
+
+function comparePrice(product1, product2) {
+  if (product1.price > product2.price) {
+    return product2
+  }else {
+    return product1
+  }
+}
