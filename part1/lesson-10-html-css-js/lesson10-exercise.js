12de5c3bdcfbeec3e702b9429e582d24c4a9ce30
02-ahmed <ahmedboabae2@gmail.com>
2024-03-03 11:15:00 +0000

    completed exercise 10d

--- part1/lesson-10-html-css-js/lesson10-exercise.js
+++ part1/lesson-10-html-css-js/lesson10-exercise.js
@@ -1,3 +1,13 @@
 const buttonElement = document.querySelector('.js-button');
 
 console.log(buttonElement.classList.contains('js-button'));
+
+
+function buttonSwitch() {
+  const jsButton = document.querySelector('.js-game-button');
+  if (jsButton.classList.contains('is-toggled') === false) {
+    jsButton.classList.add('is-toggled');
+  }else {
+    jsButton.classList.remove('is-toggled');
+  }
+}
