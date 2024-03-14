cd20fa92751083d816312c91c65b27f8f5282223
02-ahmed <ahmedboabae2@gmail.com>
2024-03-14 13:22:43 +0000

    modified function in code to use arrow function

--- part1/lesson-6-booleans/main.js
+++ part1/lesson-6-booleans/main.js
@@ -27,7 +27,7 @@ let intervalId;
 
 function autoPlay() {
   if (!isAutoPlaying) {
-    intervalId = setInterval(function() {
+    intervalId = setInterval(() => {
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
