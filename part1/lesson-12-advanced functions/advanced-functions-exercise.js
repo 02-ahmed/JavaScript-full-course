13396b895b60cc27e755ac045fcb697b6b8b023b
02-ahmed <ahmedboabae2@gmail.com>
2024-03-16 23:29:25 +0000

    completed exercise 12k

--- part1/lesson-12-advanced functions/advanced-functions-exercise.js
+++ part1/lesson-12-advanced functions/advanced-functions-exercise.js
@@ -1,5 +1,10 @@
-const multiply = (num1, num2) => {
+/* const multiply = (num1, num2) => {
   return num1 * num2;
 }
+console.log(multiply(2, 3));
+console.log(multiply(7, 10)); */
+
+//exercise 12k
+const multiply = (num1, num2) => num1 * num2;
 console.log(multiply(2, 3));
 console.log(multiply(7, 10));
