b0561a0c1b98c104c988b1b88c4e9073f0ed4d0d
02-ahmed <ahmedboabae2@gmail.com>
2024-03-16 23:52:26 +0000

    completed exercise 12m

--- part1/lesson-12-advanced functions/advanced-functions-exercise.js
+++ part1/lesson-12-advanced functions/advanced-functions-exercise.js
@@ -23,3 +23,10 @@ function countPositive(nums) {
 }
 console.log(countPositive([1, -3, 5]));
 console.log(countPositive([-2, 3, -5, 7, 10]));
+
+//exercise 12m
+function addNum(array, num) {
+  return array.map(value => value + num)
+}
+console.log(addNum([1, 2, 3], 2));
+console.log(addNum([-2, -1, 0, 99], 2));
