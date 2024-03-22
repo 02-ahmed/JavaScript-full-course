907afc5022c4a0585e5921f83f26e302e847a0b3
02-ahmed <ahmedboabae2@gmail.com>
2024-03-22 20:07:58 +0000

    added check boxes to the todo list project

--- part1/lesson-11-array-and-loops/todo-list.js
+++ part1/lesson-11-array-and-loops/todo-list.js
@@ -1,5 +1,3 @@
-
-
 let todoList = JSON.parse(localStorage.getItem('todoList'))|| [];
 
 renderTodoList();
@@ -8,26 +6,42 @@ function renderTodoList() {
   let todoListHTML = ''
 
   todoList.forEach((todoObject, index) => {
-  const {name, dueDate} = todoObject;
+  const {name, dueDate, isChecked} = todoObject;
   const html = `
-      <div>${name}</div
+      <div class="todo-name ${isChecked ? 'check' : ''}"><div class="checkbox"></div>${name}</div>
       <div>${dueDate}</div>
       <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
   todoListHTML += html;
+  })
   localStorage.setItem("todoList", JSON.stringify(todoList));
-  })
 
 
-document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
-document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
-  deleteButton.addEventListener('click', () => {
-    todoList.splice(index, 1);
-    renderTodoList();
-  })
-})
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index, 1);
+      renderTodoList();
+    })
+  });
+
+  document.querySelectorAll('.todo-name').forEach((todo, index) => {
+    const todoObject = todoList[index]
+    todo.addEventListener('click', () => {
+      todoObject.isChecked = !todoObject.isChecked;
+      renderTodoList();
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+
+    });
+  });
+  localStorage.setItem("todoList", JSON.stringify(todoList));
+  console.log(todoList);
+  
 }
+
+
+
 
 document.querySelector('.js-add-todo-button').addEventListener('click', () => {
   addTodo();
@@ -37,14 +51,15 @@ function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
 
-const dateInputElement = document.querySelector('.js-due-date-input');
-const dueDate = dateInputElement.value;
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
 
   todoList.push({
     //name: name,
     //dueDate: dueDate,
     name,
-    dueDate
+    dueDate,
+    isChecked: false
   });
 
   localStorage.setItem("todoList", JSON.stringify(todoList));
