48031c12f713add9fe5cdcd45c597ddacf3056fc
02-ahmed <ahmedboabae2@gmail.com>
2024-03-26 14:15:25 +0000

    used product ID instead of name

--- part2/javascript-amazon-project/scripts/amazon.js
+++ part2/javascript-amazon-project/scripts/amazon.js
@@ -84,7 +84,7 @@ products.forEach((product) => {
     </div>
 
     <button class="add-to-cart-button button-primary js-add-to-cart" 
-    data-product-name="${product.name}">
+    data-product-id="${product.id}">
       Add to Cart
     </button>
   </div>
@@ -97,21 +97,21 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    const productName =button.dataset.productName;
+    const productId =button.dataset.productId;
 
     let matchingItem;
 
     cart.forEach((item) => {
-      if (productName === item.productName) {
+      if (productId === item.productId) {
         matchingItem = item;
       }
-    });
+    }); 
 
     if (matchingItem) {
       matchingItem.quantity++;
     }else {
       cart.push({
-        productName: productName,
+        productId: productId,
         quantity: 1,
       });
     }
