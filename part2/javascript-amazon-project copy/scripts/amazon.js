912292edab43b3fc77c0ca2eac9ca56de28150e2
02-ahmed <ahmedboabae2@gmail.com>
2024-03-27 20:51:12 +0000

    completed exercise 13a - 13e by modifiying the code to increase quantity by the number in the selector

--- part2/javascript-amazon-project copy/scripts/amazon.js
+++ part2/javascript-amazon-project copy/scripts/amazon.js
@@ -62,7 +62,7 @@ products.forEach((product) => {
     </div>
 
     <div class="product-quantity-container">
-      <select>
+      <select class="js-quantity-selector-${product.id}">
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -107,12 +107,16 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       }
     }); 
 
+    let quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`)
+    let quantityValue = parseInt(quantitySelector.value)
+
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity+=quantityValue;
+      console.log(matchingItem)
     }else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantityValue,
       });
     }
 
