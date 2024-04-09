80a789b0b447d2acf96120e9355a436175546eee
02-ahmed <ahmedboabae2@gmail.com>
2024-04-09 13:50:25 +0000

    completed challenge exercise 15l

--- part2/javascript-amazon-project copy/scripts/checkout/orderSummary.js
+++ part2/javascript-amazon-project copy/scripts/checkout/orderSummary.js
@@ -3,9 +3,10 @@ import {products, getProduct} from '../../data/products.js';
 import  formatCurrency  from '../utils/money.js';
 import { calculateCartQuantity } from '../../data/cart.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption, calculateDeliveryDate } from '../../data/deliveryOptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 import { renderCheckoutHeader } from './checkoutHeader.js';
+
 
 
 export function renderOrderSummary() {
@@ -22,13 +23,7 @@ export function renderOrderSummary() {
 
     const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-    const today = dayjs();
-    const deliveryDate = today.add(
-      deliveryOption.deliveryDays, 'days'
-    );
-    const dateString = deliveryDate.format(
-      'dddd, MMMM D'
-    );
+    const dateString = calculateDeliveryDate(deliveryOption);
 
 
     cartSummaryHTML += `
@@ -80,13 +75,7 @@ export function renderOrderSummary() {
     let html = '';
 
     deliveryOptions.forEach((deliveryOption) => {
-      const today = dayjs();
-      const deliveryDate = today.add(
-        deliveryOption.deliveryDays, 'days'
-      );
-      const dateString = deliveryDate.format(
-        'dddd, MMMM D'
-      );
+      const dateString = calculateDeliveryDate(deliveryOption)
 
       const priceString = deliveryOption.priceCents === 0
       ? 'FREE'
@@ -161,6 +150,9 @@ export function renderOrderSummary() {
 
       updateQuantity(productId, inputValue);
       updateCartQuantity();
+      renderOrderSummary();
+      renderPaymentSummary();
+      renderCheckoutHeader();
 
       const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`).innerHTML = inputValue;
       
@@ -182,6 +174,9 @@ export function renderOrderSummary() {
 
         updateQuantity(productId, inputValue);
         updateCartQuantity();
+        renderOrderSummary();
+        renderPaymentSummary();
+        renderCheckoutHeader();
 
         const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`).innerHTML = inputValue;
         };
